5fbf3620bdbc877d3c90b2295b4e146c83892a7c
llaguno-zara <llaguno.zc@obmontessori.edu.ph>
2026-09-17 00:59:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pyscript import document, display
 
+tax = 0.12
+subtax = tax * subtotal
+grandtot = subtotal + price
+
 def add_num(e): #put e for the event handler
     document.getElementById("output1").innerHTML = "" #clears previous result
     num1 = float(document.getElementById("input1").value) #get input value
@@ -26,10 +30,18 @@ def place_order(e):
 
 def show_order(e):
     document.getElementById("output4").innerHTML = "" #clears previous result
-    prod1=document.getElementById("item1") #get item 1 id
+    prod1=document.getElementById("prod1") #get item 1 id
     subtotal = float(prod1.value) * prod1.checked
     size = document.querySelector("input[name= 'size']: checked")
     price = float(size.value)
     grandtotal = subtotal + price
     final_order = grandtotal + coffee_price
-    display(f'You have to pay a total of {final_order}', target="output4") No newline at end of file
+    display(f'You have to pay a total of {final_order}', target="output4")
+
+subtotal = (float(prod1.value) * prod1.checked + float(prod2.value) * prod2.checked + float(prod3.value) * prod3.checked + float(prod4.value) * prod4.checked
+)
+
+display(f"Subtotal: {Subtotal}.", target="output1")
+display(f"Tax: {tax}.", target="output1", append=True)
+display(f"Total: {total}.", target="output1", append=True)
+display("Thank you for your order!", target="output1", append=True)
